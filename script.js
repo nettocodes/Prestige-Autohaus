70c8d39bcabc80381c4057827d011aa928f2bd5a
IvoBraatz <braatzivo@hotmail.com>
2023-12-27 23:34:57 -0300

    add porshce and ferrari

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
-// const cars = document.querySelectorAll('.inventory .car');
-
-// const updateCarRotation = (event) => {
-//     cars.forEach((car) => {
-//         const rect = car.getBoundingClientRect();
-//         const centerX = rect.left + rect.width / 2;
-//         const centerY = rect.top + rect.height / 2;
-//         const mouseX = event.clientX - centerX;
-//         const mouseY = event.clientY - centerY;
-//         const maxRotation = 15;
-
-//         const rotateY = (mouseX / centerX) * maxRotation;
-//         const rotateX = -(mouseY / centerY) * maxRotation;
-
-//         if (
-//             event.target === car ||
-//             event.target.closest('.car') === car
-//         ) {
-//             car.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-//         } else {
-//             car.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
-//         }
-//     });
-// };
-
-// const resetCarRotation = () => {
-//     cars.forEach((car) => {
-//         car.style.transform = 'perspective(1000px) rotateX(0) rotateY(0)';
-//     });
-// };
-
-// document.addEventListener("mousemove", updateCarRotation);
-// document.querySelector('.inventory').addEventListener("mouseleave", resetCarRotation);
+// Função para criar e adicionar imagens ao slider
+function populateSliderWithImages(brand, model, totalImages) {
+    const slider = document.getElementById(`${brand}-slider`);
+    for (let i = 1; i <= totalImages; i++) {
+      const imageDiv = document.createElement('div');
+      imageDiv.innerHTML = `<img class="inventory__image" src="src/img/cars/${brand}/${model}/${model}_${i}.jpg" alt="Image ${i}">`;
+      slider.appendChild(imageDiv);
+    }
+  }
+  
+  // Adicionar imagens para modelos específicos de BMW e Audi
+  populateSliderWithImages('bmw', 'm8', 14); // Altere para o número total de imagens para o modelo BMW M8
+  populateSliderWithImages('audi', 'rs6', 19); // Altere para o número total de imagens para o modelo Audi RS6
+  populateSliderWithImages('porsche', '911', 15);
+  populateSliderWithImages('ferrari', 'sf90', 20)
+  
