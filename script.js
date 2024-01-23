3e72c5e9617ab03cb33108d04a11851266ced45e
Ivo Braatz <braatzivo@hotmail.com>
2024-01-22 22:20:23 -0300

    alterado responsividade e backgrounds automaticos

--- script.js
+++ script.js
@@ -30,3 +30,55 @@ function populateSliderWithImages(brand, model, totalImages) {
       }
     });
   });
+
+  function reiniciarVideo() {
+    var video = document.querySelector('.video__banner');
+    video.currentTime = 0; // Reinicia o vídeo para o início
+    video.play(); // Inicia a reprodução novamente
+  }
+
+  function verificarTempoLimite() {
+    var video = document.querySelector('.video__banner');
+    var tempoLimite = 28; // Defina o tempo máximo desejado em segundos
+
+  if (video.currentTime >= tempoLimite) {
+      video.pause();
+      video.currentTime = 0;
+      reiniciarVideo();
+  }
+}
+
+function getRandomImages(brand, model, totalImages) {
+  const randomIndexes = [];
+  while (randomIndexes.length < 3) {
+    const randomIndex = Math.floor(Math.random() * totalImages) + 1;
+    if (!randomIndexes.includes(randomIndex)) {
+      randomIndexes.push(randomIndex);
+    }
+  }
+  return randomIndexes.map(index => `src/img/cars/${brand}/${model}/${model}_${index}.jpg`);
+}
+
+function populateBackgroundWithImages(brand, models, totalImages) {
+  const backgroundDiv = document.querySelector(`.brand.${brand} .background`);
+
+  models.forEach(model => {
+    const randomImages = getRandomImages(brand, model, totalImages);
+
+    randomImages.forEach((imageUrl, index) => {
+      const imageDiv = document.createElement('div');
+      imageDiv.classList.add(`back`, `${brand}_${model.toLowerCase()}`);
+      imageDiv.style.backgroundImage = `url(${imageUrl})`;
+      imageDiv.style.flex = '1'; // Cada imagem ocupará 1/3 do espaço
+      backgroundDiv.appendChild(imageDiv);
+    });
+  });
+}
+
+// Adicione mais modelos e marcas conforme necessário
+populateBackgroundWithImages('bmw', ['m8'], 14);
+populateBackgroundWithImages('audi', ['rs6'], 19);
+populateBackgroundWithImages('porsche', ['911'], 15);
+populateBackgroundWithImages('ferrari', ['sf90'], 20);
+populateBackgroundWithImages('lamborghini', ['huracan'], 10);
+populateBackgroundWithImages('chevrolet', ['silverado'], 11);
