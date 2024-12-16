eea93e8aea5c0bf0c4b1907746e7d1243651767d
IvoBraatz <braatzivo@hotmail.com>
2024-12-16 11:31:23 -0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Vehicle Management API!');
 });
 
-// Configuração da porta do servidor
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
