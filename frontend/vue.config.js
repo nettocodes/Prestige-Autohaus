03393f2499ad1abf8e4494b001cfed17edb3d8b0
IvoBraatz <braatzivo@hotmail.com>
2025-01-25 12:22:45 -0300

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,18 +1,4 @@
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new BundleAnalyzerPlugin(), // Analise o tamanho do bundle
-    ],
-    optimization: {
-      splitChunks: {
-        chunks: "all",
-        minSize: 20000, // Tamanho mínimo para divisão
-        maxSize: 50000, // Tamanho máximo para divisão
-        automaticNameDelimiter: "-",
-      },
-    },
-  },
-  productionSourceMap: false, // Remove source maps na produção
-};
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
